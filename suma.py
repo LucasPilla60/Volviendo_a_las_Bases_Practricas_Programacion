fa71672196fea7c2534e0e1bcd699e795225fed8
LucasPilla60 <lpilla60@gmail.com>
2025-01-04 20:10:48 -0300

    Primer commit: Agregando sumapy para prácticas de programación

---
+++ suma.py
@@ -0,0 +1,14 @@
+# Programa para sumar 2 numeros 
+
+# Solicitud de numeros al usuario
+
+num1 = int(input("Ingrese el primer numero: "))
+num2= int(input("Ingrese el segundo numero:"))
+
+# Suma de los numeros
+
+resultado = num1 + num2
+
+# Mostrar el resultado
+
+print(f"La suma de {num1} y {num2} es {resultado}")
