4bad93886901a994baa08dfe6cced672181c65ff
LucasPilla60 <lpilla60@gmail.com>
2025-01-04 20:46:21 -0300

    Añadido programa para multiplicar 2 numeros

---
+++ multiplicacion.py
@@ -0,0 +1,14 @@
+# Programa para multiplicar 2 numeros
+
+# Solicitud de numeros al usuario
+
+num1 = int(input("Ingrese el primer numero: "))
+num2= int(input("Ingrese el segundo numero:"))
+
+# Multiplicacion de los numeros
+
+resultado = num1 * num2
+
+# Mostrar el resultado
+
+print(f"La multiplicacion de {num1} y {num2} es {resultado}")
