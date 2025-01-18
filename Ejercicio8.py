9cc23df929f1edf92dc94e5f72aa38dad78417db
LucasPilla60 <lpilla60@gmail.com>
2025-01-18 16:45:33 -0300

    Modificaciones

--- Ejercicio8.py
+++ Ejercicio8.py
@@ -1,6 +1,6 @@
 # Crea un diccionario para almacenar los datos de una persona (nombre, edad, ciudad) e imprime sus valores.
 
-Datos = {"nombre": "Lucas", "edad": 33, "ciudad": "Cordoba"}
+Datos = {"nombre": "Lucas", "edad": 33 , "ciudad": "Cordoba"}
 
 
 
