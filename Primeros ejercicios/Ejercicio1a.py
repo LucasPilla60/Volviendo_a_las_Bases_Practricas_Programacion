6c1828f7030bdb4c60417a678d4e4615ee07a9c3
LucasPilla60 <lpilla60@gmail.com>
2025-01-20 15:57:27 -0300

    Agregado mas ejercicios

---
+++ Primeros ejercicios/Ejercicio1a.py
@@ -0,0 +1,14 @@
+# Realiza un programa que lea cuatro valores numeros e informar su suma y promedio.
+
+numer1 = int(input("Ingrese el primer numero: "))
+numer2 = int(input("Ingrese el segundo numero: "))
+numero3 = int(input("Ingrese el tercer numero: "))
+numero4 = int(input("Ingrese el cuarto numero: "))
+
+suma = numer1 + numer2 + numero3 + numero4
+promedio = suma /4
+
+print(f"La suma de los numeros es: {suma}")
+print(f"El promedio es {promedio}")
+
+
---
+++ Primeros ejercicios/Ejercicio2a.py
@@ -0,0 +1,10 @@
+# Calcular el sueldo mensual de un operario conociendo la cantidad de horas trabajadas y el valor de la hora.
+
+horas_trabajadas = int(input("Ingrese la cantidad de horas trabajadas: "))
+valor_hora = int(input("Ingrese el valor de la hora: "))
+
+sueldo = horas_trabajadas * valor_hora
+
+print(f"El sueldo mensual es: {sueldo}")
+
+
