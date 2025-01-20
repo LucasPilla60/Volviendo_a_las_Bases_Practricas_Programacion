7b32237e8fb5c809084843dafb8bdcfeb4e9a69f
LucasPilla60 <lpilla60@gmail.com>
2025-01-20 11:41:37 -0300

    Actulizacion de ejercicios

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -6,25 +6,35 @@
 
 saldo = 200000
 
+opciones = {
+    "1": lambda: print(f"El saldo actual es: {saldo}"),
+    "2": lambda: depositar(),
+    "3": lambda: retirar(),
+    "4": lambda: exit()
+}
+
+def depositar():
+    global saldo
+    deposito = float(input("Ingrese la cantidad a depositar: "))
+    saldo += deposito
+    print(f"El saldo actual es: {saldo}")
+
+def retirar():
+    global saldo
+    retiro = float(input("Ingrese la cantidad a retirar: "))
+    if retiro > saldo:
+        print("Fondos insuficientes.")
+    else:
+        saldo -= retiro
+        print(f"El saldo actual es: {saldo}")
+
 while True:
     print("1. Consultar saldo")
     print("2. Depositar dinero")
     print("3. Retirar dinero")
     print("4. Salir")
     opcion = input("Ingrese una opcion: ")
-    if opcion == "1":
-        print(f"El saldo actual es: {saldo}")
-    elif opcion == "2":
-        deposito = float(input("Ingrese la cantidad a depositar: "))
-        saldo += deposito
-        print(f"El saldo actual es: {saldo}")
-    elif opcion == "3":
-        retiro = float(input("Ingrese la cantidad a retirar: "))
-        saldo -= retiro
-        print(f"El saldo actual es: {saldo}")
-    elif opcion == "4":
-        break
-    else:
-        print("Opcion no valida")
+    accion = opciones.get(opcion, lambda: print("Opcion no valida"))
+    accion()
 
 
