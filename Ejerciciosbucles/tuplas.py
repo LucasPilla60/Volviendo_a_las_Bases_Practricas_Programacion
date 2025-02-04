42c474e1dbdc052246e4e681ee9ff03c5ca38c9d
LucasPilla60 <lpilla60@gmail.com>
2025-02-04 12:30:35 -0300

    Tuplas

---
+++ Ejerciciosbucles/tuplas.py
@@ -0,0 +1,14 @@
+my_location = (-31.4135, -64.18105)
+
+friend1_location = (37.7749, -122.4194)    # San Francisco
+friend2_location = (40.7128, -74.0060)     # New York
+friend3_location = (4.624335, -74.063644)  # Bogota
+
+friends = (
+    (37.7749, -122.4194),    # San Francisco
+    (40.7128, -74.0060),     # New York
+    (4.624335, -74.063644)   # Bogota
+)
+all_friends_location = (friend1_location + friend2_location + friend3_location)
+
+print(all_friends_location)
