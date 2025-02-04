8a793fe3aabbcd00303f85a445d22cf032c4d2c4
LucasPilla60 <lpilla60@gmail.com>
2025-02-04 13:40:25 -0300

    Usando sets

---
+++ Ejerciciosbucles/fruit_sets.py
@@ -0,0 +1,8 @@
+set1 = {'manzana', 'banana', 'durazno', 'pera', 'uva'}
+set2 = {'damazco', 'anana', 'durazno', 'pera', 'uva'}
+        
+# 1. ¿Qué frutas están en ambos sets?
+
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2))
